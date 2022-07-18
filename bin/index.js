60ecbe07a26758e6e74b56f8279d8d66d598851e
Abdul Rafay <farhatiqbal@protonmail.com>
2022-07-18 19:23:40 +0500

    Initial Commit :tada:

---
+++ bin/index.js
@@ -0,0 +1,54 @@
+#! /usr/bin/env node
+const fs = require('fs')
+var args = require('minimist')(process.argv.slice(2),{
+    string:["fonts"]
+});
+let font_path = '/assets/fonts';
+let fonts = process.cwd()+font_path;
+if(args['fonts']!==undefined){
+    font_path = args.fonts;
+    fonts =  process.cwd()+font_path;
+}
+
+try {
+  if (!fs.existsSync(fonts)) {
+    console.error("Font Directory does not exist.")
+    process.exit(1);
+  }
+} catch(e) {
+  console.error("An error occurred.",e);
+  process.exit(1);
+
+}
+
+let reactNativeConfigFile = `module.exports = {
+    project: {
+      ios: {},
+      android: {},
+    },
+    assets: ['.${font_path}'],
+  };
+  `;
+
+fs.writeFile('react-native.config.js', reactNativeConfigFile, function (err) {
+    if (err){
+        throw err;
+    }
+    console.log('react-native.config.js is created.');
+  });
+
+  const { exec } = require('child_process');
+exec('npx react-native link', (err, stdout, stderr) => {
+  if (err) {
+    console.error("Unable to execute the command npx react-native link");
+    // node couldn't execute the command
+    return;
+  }
+
+  console.log(`${stdout}`);
+  if(stderr!==''){
+    console.error(`Error: ${stderr}`);
+  }
+});
+
+
---
+++ readme.md
@@ -0,0 +1,18 @@
+# react-native-font-link
+
+A Simple npx Script That Automates Linking of Fonts to Your React Native No Extra Dependencies
+to install just run the npx command inside your project directory to link your fonts to your react native app
+
+The React Native Font Link Library assumes the default directory to be /assets/fonts if not you can specify your own by providing --fonts
+
+## Usage
+```
+
+npx react-native-font-link
+//Specifiying Font Directory
+npx react-native-font-link --fonts="/custom_assets/fonts"
+
+```
+
+## License
+[MIT](https://choosealicense.com/licenses/MIT/)
